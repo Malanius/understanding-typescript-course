b25fee75c6a23a176ba32d1d9d9df7b27e3befc7
Malanius Privierre <malaniusprivierre@gmail.com>
2020-07-26 18:59:26 +0200

    Literal types: another combine result example

--- examples/02-types/literalTypes copy.ts
+++ examples/02-types/literalTypes copy.ts
@@ -4,16 +4,17 @@ const literalCombine = (
   resultConversion: string
 ) => {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  if (resultConversion === "as-number") {
-    return +result;
-  } else {
-    return result.toString();
-  }
+  // if (resultConversion === "as-number") {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
+  return result;
 };
 
 const literalCombineNums = literalCombine(30, 26, "as-number");
