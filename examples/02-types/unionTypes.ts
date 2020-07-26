af512a7e56d9abfeb5a48bf37355a74ae2a3c20c
Malanius Privierre <malaniusprivierre@gmail.com>
2020-07-26 18:33:39 +0200

    Union type: initial example

---
+++ examples/02-types/unionTypes.ts
@@ -0,0 +1,10 @@
+const combine = (input1: number, input2: number) => {
+  const result = input1 + input2;
+  return result;
+};
+
+const combineNumbers = combine(30,26);
+console.log(combineNumbers);
+
+const combineNames = combine('Java', 'Script');
+console.log(combineNames);
