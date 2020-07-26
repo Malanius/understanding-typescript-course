11d6c0a461af3f9c584ea980d96d1d8f2e6242fb
Malanius Privierre <malaniusprivierre@gmail.com>
2020-07-26 17:42:47 +0200

    Array: create fixed type array

---
+++ examples/02-types/arrays.ts
@@ -0,0 +1,9 @@
+const betterPerson = {
+  name: "Malanius",
+  age: 30,
+  hobbies: ["programming", "moutain biking"],
+};
+
+let favoriteActivities: string [];
+
+console.log(betterPerson.hobbies);
