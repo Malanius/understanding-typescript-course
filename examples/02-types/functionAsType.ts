8eb01d0623b57b7b7ad11152d611c76a86023362
Malanius Privierre <malaniusprivierre@gmail.com>
2020-07-26 19:36:44 +0200

    Function type: using bare Function type

--- examples/02-types/functionAsType.ts
+++ examples/02-types/functionAsType.ts
@@ -3,4 +3,7 @@ const addFn = (n1: number, n2: number) => n1 + n2;
 let combineValues: Function;
 
 combineValues = addFn;
+combineValues = () => {
+  console.log("Trolled");
+};
 console.log(combineValues(8, 8));
