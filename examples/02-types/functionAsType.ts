0b0e34af42110d8c79f0a4758e6b78cdc8e4ef43
Malanius Privierre <malaniusprivierre@gmail.com>
2020-07-26 19:35:13 +0200

    Function type: specifying function type

--- examples/02-types/functionAsType.ts
+++ examples/02-types/functionAsType.ts
@@ -1,7 +1,6 @@
 const addFn = (n1: number, n2: number) => n1 + n2;
 
-let combineValues; //any
+let combineValues: Function;
 
 combineValues = addFn;
-combineValues = 5;
 console.log(combineValues(8, 8));
