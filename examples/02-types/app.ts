c52d977017dd8f4e848a4688d6ee1851143edd41
Malanius Privierre <malaniusprivierre@gmail.com>
2020-07-26 17:09:15 +0200

    Boolean: add boolean parameter to add function

--- examples/02-types/app.ts
+++ examples/02-types/app.ts
@@ -1,7 +1,14 @@
-const addNumbers = (n1: number, n2: number) => n1 + n2;
+const addNumbers = (n1: number, n2: number, showResult: boolean) => {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(result);
+  } else {
+    return result;
+  }
+};
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
 
-const result = addNumbers(number1, number2);
-console.log(result);
+const result = addNumbers(number1, number2, printResult);
