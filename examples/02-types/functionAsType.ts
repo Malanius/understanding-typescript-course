0ca86bb73108f8936018438638aa8cdd43a9269e
Malanius Privierre <malaniusprivierre@gmail.com>
2020-07-26 19:38:46 +0200

    Function type: specifying whole function type (args and return type)

--- examples/02-types/functionAsType.ts
+++ examples/02-types/functionAsType.ts
@@ -1,9 +1,9 @@
 const addFn = (n1: number, n2: number) => n1 + n2;
 
-let combineValues: Function;
+let combineValues: (a: number, b: number) => number;
 
 combineValues = addFn;
-combineValues = () => {
-  console.log("Trolled");
-};
+// combineValues = () => {
+//   console.log("Trolled");
+// };
 console.log(combineValues(8, 8));
