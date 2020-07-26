4097948561b2c95d5283206523cebbd9fa6c9362
Malanius Privierre <malaniusprivierre@gmail.com>
2020-07-26 17:10:18 +0200

    String: add string parameter to add function

--- examples/02-types/app.ts
+++ examples/02-types/app.ts
@@ -1,7 +1,12 @@
-const addNumbers = (n1: number, n2: number, showResult: boolean) => {
+const addNumbers = (
+  n1: number,
+  n2: number,
+  showResult: boolean,
+  message: string
+) => {
   const result = n1 + n2;
   if (showResult) {
-    console.log(result);
+    console.log(message + result);
   } else {
     return result;
   }
@@ -10,5 +15,6 @@ const addNumbers = (n1: number, n2: number, showResult: boolean) => {
 const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
+const resultPhrase = "Result is: ";
 
-const result = addNumbers(number1, number2, printResult);
+const result = addNumbers(number1, number2, printResult, resultPhrase);
