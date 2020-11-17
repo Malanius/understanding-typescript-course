224cf3db87c63f2a39c750ac35e964a425717199
Malanius Privierre <malaniusprivierre@gmail.com>
2020-11-17 12:52:00 +0100

    Classes: use private access modifier

--- examples/05-classes-interfaces/src/app.ts
+++ examples/05-classes-interfaces/src/app.ts
@@ -1,17 +1,38 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(name: string) {
     this.name = name;
   }
 
-  describe(this: Department) { //dummy parameter for TS
+  //this is dummy parameter for TS
+  describe(this: Department) {
     console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployees() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 const accounting = new Department("Accounting");
-accounting.describe();
 
-const accountingCopy = {describe: accounting.describe};
+accounting.addEmployee('Larien');
+accounting.addEmployee('Naerlien');
+
+accounting.describe();
+accounting.printEmployees();
+
+// possible as by default all is public
+// impossible when employees are private
+// accounting.employees[2] = 'Masomleyn'; 
+
+// const accountingCopy = { describe: accounting.describe };
 // accountingCopy.describe(); // compilation error as not an instance of Department
+
