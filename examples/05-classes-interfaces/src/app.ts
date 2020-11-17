ed860b8dedd32e06ce2d88cb3e1f003292460bad
Malanius Privierre <malaniusprivierre@gmail.com>
2020-11-17 12:40:03 +0100

    Classes: this parameter

--- examples/05-classes-interfaces/src/app.ts
+++ examples/05-classes-interfaces/src/app.ts
@@ -4,7 +4,14 @@ class Department {
   constructor(name: string) {
     this.name = name;
   }
+
+  describe(this: Department) { //dummy parameter for TS
+    console.log("Department: " + this.name);
+  }
 }
 
 const accounting = new Department("Accounting");
-console.log(accounting);
+accounting.describe();
+
+const accountingCopy = {describe: accounting.describe};
+// accountingCopy.describe(); // compilation error as not an instance of Department
